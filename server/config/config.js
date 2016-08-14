87aab29d99d9d18d151503adddcc7b0bc8dc0a46
ahansb <andrejboyadjiev@gmail.com>
2016-08-14 14:23:13 +0300

    changing connection string

--- server/config/config.js
+++ server/config/config.js
@@ -5,7 +5,7 @@ var rootPath = path.normalize(__dirname + '/../../');
 module.exports={
   development:{
       rootPath:rootPath,
-      db:'mongodb://localhost:27017/stangasystem',
+      db:'mongodb://heroku_3msv4gx5:416diued9b6nh1st3f1lgsso6h@ds047945.mlab.com:47945/heroku_3msv4gx5',
       port: process.env.PORT || 3000
   }
 };
